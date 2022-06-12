276cb2009029ea70f88312b385e45a2c7d9c3bf8
Ben Cullen <benjaminjcullen1@gmail.com>
2022-06-12 15:16:58 +0100

    changed all content to props

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
+import faker from "faker";
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Sam" />
-            <CommentDetail author="Joe" />
-            <CommentDetail author="Paul" />
-            <CommentDetail author="John" />
-            <CommentDetail author="Mark" />
+            <CommentDetail author="Sam" timeAgo="Today at 4:45PM" comment="Some Comment" image={faker.image.image()}/>
+            <CommentDetail author="Joe" timeAgo="Today at 1:30PM" comment="Another Comment" image={faker.image.image()}/>
+            <CommentDetail author="Paul" timeAgo="Yesterday at 1:00AM" comment="Even more comments" image={faker.image.image()}/>
         </div>
     );
 };
